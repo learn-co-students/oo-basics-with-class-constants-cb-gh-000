c58d53c8df51eaa948f5414ab9f81909502c28a3
Eearslya Sleiarion <eearslya@gmail.com>
2017-02-09 06:17:04 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,18 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    GENRES << genre unless GENRES.include?(genre)
   end
 
 end
