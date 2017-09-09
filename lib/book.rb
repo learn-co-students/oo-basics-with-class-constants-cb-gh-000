56aaf0a4e1461a01496b91f03f4659f189266248
Colin McKean <ezikill@hotmail.com>
2017-09-09 18:25:15 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +11,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
