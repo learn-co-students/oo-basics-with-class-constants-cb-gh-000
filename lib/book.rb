4a9ec44cc5023d54fcaccc35797ede4468095d94
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-25 23:03:19 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,14 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  GENRES = []
   def initialize(title)
     @title = title
+  end
+
+  def genre=(x)
+    @genre = x
+    GENRES << x
   end
 
   def turn_page
