c3bf53c4b7de46f19a1c1db3046f2077ee1016f0
VISHAL RAJ <sunilkroy1973@gmail.com>
2018-06-07 07:23:34 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,16 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand) == false 
+      BRANDS << brand
+    end 
   end
+  
+  BRANDS = []
+  
+  def brand=(brand)
+    @brand = brand 
+  end 
 
   def cobble
     self.condition = "new"
