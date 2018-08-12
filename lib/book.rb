40685265036205db99ecf1765322a045e655010f
Shane Glass <shaneryanglass88@gmail.com>
2018-08-12 23:37:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,21 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
+  GENRES = []
+  
   def initialize(title)
     @title = title
+    
   end
-
+  
+  def genre=(genre)
+    @genre = genre
+    unless GENRES.include?(genre)
+     GENRES << genre
+    end
+  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
+  
 
   def initialize(brand)
     @brand = brand
+    unless BRANDS.include?(brand)
+     BRANDS << brand
+    end
   end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
