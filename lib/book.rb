9e8bf617c49cb8a697f341a292c623d6f675eb18
Lukas F <lukas.forst48@googlemail.com>
2018-09-17 09:48:10 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,9 @@
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = [] # an empty array
 
   def initialize(title)
     @title = title
@@ -8,6 +11,11 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre) #writer/setter method
+    @genre = genre
+    GENRES << genre #adds genre to the GENRES class constant array
   end
 
 end
