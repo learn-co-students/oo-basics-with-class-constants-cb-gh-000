0acdd77b6aff27468fcc4aaeb4b235242add7e60
Carter Van Ekeren <vanek015@umn.edu>
2018-11-16 03:50:00 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,20 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
+  attr_reader :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    if @genre
+      GENRES.delete(@genre)
+    end
+    GENRES << genre
+    @genre = genre
   end
 
   def turn_page
