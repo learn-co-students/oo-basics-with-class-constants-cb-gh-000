629663bb8a3dd9b370dfda03407abcdb2c4d1696
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 19:21:44 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  GENRES = []  # a class constant  Book::GENRES
 
   def initialize(title)
     @title = title
@@ -8,6 +9,11 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)  # write the genre to a constant 'GENRE'
+    @genre = genre  # save an instance on the genre variable to be able to modify it
+    GENRES << genre  # add the current book's genre to the constant GENRES to keep track
   end
 
 end
